f74cd3701ff90b7db89ed6e4ab5ff7fb89654403
Le-dev-du-coin <ledevducoin223@gmail.com>
2024-09-27 19:51:23 +0000

    Change in escompte_ristoune Module to calculate escompte and ristourne for partner individualy

--- phoenix_escompte_ristourne/models/escompte_ristourne.py
+++ phoenix_escompte_ristourne/models/escompte_ristourne.py
@@ -2,53 +2,78 @@ from odoo import models, fields, api
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    limit_1 = fields.Monetary(string='Limite > 500 000 F', currency_field='currency_id', default=500000, tracking=True)
-    limit_2 = fields.Monetary(string='Limite > 1 000 000 F', currency_field='currency_id', default=1000000)
-    limit_3 = fields.Monetary(string='Limite > 2 000 000 F', currency_field='currency_id', default=2000000)
-    limit_4 = fields.Monetary(string='Limite > 4 000 000 F', currency_field='currency_id', default=4000000)
-    
+    limit_1 = fields.Monetary(
+        string="Limite > 500 000 F",
+        currency_field="currency_id",
+        default=500000,
+        tracking=True,
+    )
+    limit_2 = fields.Monetary(
+        string="Limite > 1 000 000 F", currency_field="currency_id", default=1000000
+    )
+    limit_3 = fields.Monetary(
+        string="Limite > 2 000 000 F", currency_field="currency_id", default=2000000
+    )
+    limit_4 = fields.Monetary(
+        string="Limite > 4 000 000 F", currency_field="currency_id", default=4000000
+    )
+
     # Taux escompte
-    escompte_rate_1 = fields.Float(string='Taux Escompte 1 (%)', default=1.5)
-    escompte_rate_2 = fields.Float(string='Taux Escompte 2 (%)', default=2.0)
-    escompte_rate_3 = fields.Float(string='Taux Escompte 3 (%)', default=2.5)
-    escompte_rate_4 = fields.Float(string='Taux Escompte 4 (%)', default=3.5)
+    escompte_rate_1 = fields.Float(string="Taux Escompte 1 (%)", default=1.5)
+    escompte_rate_2 = fields.Float(string="Taux Escompte 2 (%)", default=2.0)
+    escompte_rate_3 = fields.Float(string="Taux Escompte 3 (%)", default=2.5)
+    escompte_rate_4 = fields.Float(string="Taux Escompte 4 (%)", default=3.5)
     # Taux ristourne
-    ristourne_rate_1 = fields.Float(string='Taux Ristourne 1 (%)', default=1.5)
-    ristourne_rate_2 = fields.Float(string='Taux Ristourne 2 (%)', default=2.0)
-    ristourne_rate_3 = fields.Float(string='Taux Ristourne 3 (%)', default=2.5)
-    ristourne_rate_4 = fields.Float(string='Taux Ristourne 4 (%)', default=3.5)
+    ristourne_rate_1 = fields.Float(string="Taux Ristourne 1 (%)", default=1.5)
+    ristourne_rate_2 = fields.Float(string="Taux Ristourne 2 (%)", default=2.0)
+    ristourne_rate_3 = fields.Float(string="Taux Ristourne 3 (%)", default=2.5)
+    ristourne_rate_4 = fields.Float(string="Taux Ristourne 4 (%)", default=3.5)
 
     def calculate_escompte(self):
         """Calcule et applique l'escompte pour le client."""
         today = fields.Date.today()
-        first_day_of_month = today.replace(day=1)
-        report = self.env['escompte.ristourne.report'].search([('month', '=', first_day_of_month)], limit=1)
-        
+
+        for partner in self:
+            first_day_of_month = today.replace(day=1)
+
+        report = self.env["escompte.ristourne.report"].search(
+            [
+                ("month", "=", first_day_of_month),
+                ("pharmacie_id", "=", partner.company_id.id),
+            ],
+            limit=1,
+        )
+
         if not report:
-            report = self.env['escompte.ristourne.report'].create({
-                'company_id': self.env.company.id,
-                'month': first_day_of_month,
-                'total_invoices': 0,
-                'total_amount': 0,
-                'total_escompte': 0,
-                'total_ristourne': 0,
-                'currency_id': self.env.company.currency_id.id
-            })
+            report = self.env["escompte.ristourne.report"].create(
+                {
+                    "pharmacie_id": partner.id,
+                    "month": first_day_of_month,
+                    "total_invoices": 0,
+                    "total_amount": 0,
+                    "total_escompte": 0,
+                    "total_ristourne": 0,
+                    "currency_id": self.env.company.currency_id.id,
+                }
+            )
 
         for partner in self:
             # Total des factures payées avant la fin du mois
-            invoices = self.env['account.move'].search([
-                ('partner_id', '=', partner.id),
-                ('state', '=', 'posted'),
-                ('invoice_date_due', '<=', fields.Date.today()),
-                ('payment_state', '=', 'paid')
-            ])
-            total_paid = sum(invoices.mapped('amount_total'))
+            invoices = self.env["account.move"].search(
+                [
+                    ("partner_id", "=", partner.id),
+                    ("move_type", "=", "out_invoice"),
+                    ("state", "=", "posted"),
+                    ("invoice_date_due", "<=", fields.Date.today()),
+                    ("payment_state", "=", "paid"),
+                ]
+            )
+            total_paid = sum(invoices.mapped("amount_total"))
             escompte = ristourne = 0
 
-             # Appliquer les taux selon les limites atteintes
+            # Appliquer les taux selon les limites atteintes
             if total_paid >= partner.limit_4:
                 escompte = total_paid * (partner.escompte_rate_4 / 100)
                 ristourne = total_paid * (partner.ristourne_rate_4 / 100)
@@ -63,7 +88,6 @@ class ResPartner(models.Model):
                 ristourne = total_paid * (partner.ristourne_rate_1 / 100)
             else:
                 escompte = ristourne = 0
-               
 
             report.total_invoices += len(invoices)
             report.total_amount += total_paid
--- phoenix_escompte_ristourne/models/escompte_ristourne_report.py
+++ phoenix_escompte_ristourne/models/escompte_ristourne_report.py
@@ -5,7 +5,7 @@ class EscompteRistourneReport(models.Model):
     _description = 'Rapport Escompte et Ristourne'
     _rec_name = 'month'
 
-    pharmacie_id= fields.Many2one('res.company', string='Pharmacie', required=True, default=lambda self: self.env.company)
+    pharmacie_id = fields.Many2one('res.partner', string='Pharmacie', required=True, default=lambda self: self.env.company.partner_id)
     month = fields.Date(string="Mois", required=True)
     total_invoices = fields.Integer(string="Nombre de factures", readonly=True)
     total_amount = fields.Monetary(string="Montant total des factures", readonly=True, currency_field='currency_id')
